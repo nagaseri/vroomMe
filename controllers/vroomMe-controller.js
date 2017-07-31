812aec85e17e788546cfdba545a695851ac4c3ce
Chi <chulinguy@gmail.com>
2017-07-31 10:32:50 -0700

    for Eri testing; WIP

--- controllers/vroomMe-controller.js
+++ controllers/vroomMe-controller.js
@@ -3,14 +3,16 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function (req, res) {
-  var d = new Date();  
-  var daysOfWeek = [
-    {day:"M"},
-    {day:"Tu"},
-    {day:"W"},
-    {day:"Th"},
-    {day:"F"},
-    ]; 
+  // var d = new Date();
+  console.log('rendering index page')  
+  var daysOfWeek = {
+    days:[
+      {day:"M"},
+      {day:"Tu"},
+      {day:"W"},
+      {day:"Th"},
+      {day:"F"},
+    ]}; 
   res.render("index", daysOfWeek);
 });
 
@@ -26,10 +28,12 @@ router.get("/rides", function (req, res) {
   res.render("drive", hbsObject);
 });
 
+module.exports = router; 
+
 router.get("/drives", function (req, res) {
   var  hbsObject = {
     rides: [{
-      rider: 'Eri',
+      rider: 'eri',
       origin: 'oakland',
       destination: 'san francisco'
     }]
